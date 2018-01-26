dbda5e825f23ea9933fc0d345a2a819a94d47e86
Juliano Custódio <juliano.custodio@hotmail.com.br>
2018-01-26 21:03:35 -0200

    Implement Up, Left and Right Sliding

--- DemoSlidingPanel/DemoSlidingPanel/MainPage.xaml.cs
+++ DemoSlidingPanel/DemoSlidingPanel/MainPage.xaml.cs
@@ -12,17 +12,62 @@ namespace DemoSlidingPanel
         public MainPage()
         {
             InitializeComponent();
-            PageDown.TranslationY = 600;
+
+            PageDown.TranslationY = 1000;
+            PageRight.TranslationX = 1000;
+            PageLeft.TranslationX = -1000;
+            PageUp.TranslationY = -1000;
         }
 
-        async void Up_Tapped(object sender, System.EventArgs e)
+      
+       
+        async void UpBlue_Tapped(object sender, System.EventArgs e)
         {
             await PageDown.TranslateTo(0, 0, 500, Easing.SinIn);
         }
 
-        async void Down_Tapped(object sender, System.EventArgs e)
+        async void DownWhite_Tapped(object sender, System.EventArgs e)
         {
             await PageDown.TranslateTo(0, Page.Height, 500, Easing.SinIn);
         }
+
+
+
+        async void LeftBlue_Tapped(object sender, System.EventArgs e)
+        {
+            await PageRight.TranslateTo(0, 0, 500, Easing.SinIn);
+        }
+
+        async void RightWhite_Tapped(object sender, System.EventArgs e)
+        {
+            await PageRight.TranslateTo(Page.Width, 0, 500, Easing.SinIn);
+        }
+
+
+
+        async void LeftWhite_Tapped(object sender, System.EventArgs e)
+        {
+            await PageLeft.TranslateTo(-Page.Width, 0, 500, Easing.SinIn);
+        }
+        
+        async void RightBlue_Tapped(object sender, System.EventArgs e)
+        {
+            await PageLeft.TranslateTo(0, 0, 500, Easing.SinIn);
+        }
+
+
+
+
+        async void UpWhite_Tapped(object sender, System.EventArgs e)
+        {
+            await PageUp.TranslateTo(0, -Page.Height, 500, Easing.SinIn);
+        }
+
+        async void DownBlue_Tapped(object sender, System.EventArgs e)
+        {
+            await PageUp.TranslateTo(0, 0, 500, Easing.SinIn);
+        }
+        
+
     }
 }
