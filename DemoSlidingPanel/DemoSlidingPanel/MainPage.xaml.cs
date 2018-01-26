2571d6e46bd0f94436ba4514a7cc779554760122
Juliano Custódio <juliano.custodio@hotmail.com.br>
2018-01-26 01:08:12 -0200

    Implement the SlidingPanel Down

--- DemoSlidingPanel/DemoSlidingPanel/MainPage.xaml.cs
+++ DemoSlidingPanel/DemoSlidingPanel/MainPage.xaml.cs
@@ -5,13 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace DemoSlidingPanel.Views
+namespace DemoSlidingPanel
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PageDown.TranslationY = 600;
+        }
+
+        async void Up_Tapped(object sender, System.EventArgs e)
+        {
+            await PageDown.TranslateTo(0, 0, 500, Easing.SinIn);
+        }
+
+        async void Down_Tapped(object sender, System.EventArgs e)
+        {
+            await PageDown.TranslateTo(0, Page.Height, 500, Easing.SinIn);
+        }
+    }
 }
